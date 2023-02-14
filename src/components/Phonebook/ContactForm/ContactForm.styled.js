433a12e9f6999cd89a46069c89c9a8d93909c0c2
tsylepa <tsylepa@gmail.com>
2023-02-14 20:19:01 +0000

    Tasks done

---
+++ src/components/Phonebook/ContactForm/ContactForm.styled.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { Form, Field } from 'formik';
+
+export const AddContactForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  max-width: 250px;
+`;
+
+export const Input = styled(Field)`
+  display: block;
+  padding: 6px 10px;
+  font-size: 18px;
+  border-radius: 4px;
+  border: 2px solid var(--button);
+`;
+
+export const ErrorText = styled.p`
+  position: absolute;
+  margin: 0;
+  font-size: 14px;
+  color: var(--error);
+
+  ::first-letter {
+    text-transform: capitalize;
+  }
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 6px;
+  font-size: 20px;
+  background: var(--button);
+  transition: background-color var(--transition-function),
+    color var(--transition-function);
+  cursor: pointer;
+
+  :hover {
+    background: var(--button-add);
+    color: var(--contrast-color);
+  }
+`;
